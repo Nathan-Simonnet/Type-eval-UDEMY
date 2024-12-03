02669fbc84b812c15be3cfe2f166733db15518f4
Nathan-Simonnet <nathan.simonnet@gmail.com>
2024-12-03 18:22:36 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const APIEndpoint = "http://api.quotable.io/random";
+
+let quote;
+function quotesFetcher() {
+    fetch('http://api.quotable.io/random')
+        .then((response) => response.json())
+        .then((data) => {
+            quote = data.content;
+            console.log(quote);
+            textModelDisplayer();
+        })
+        .catch((error) => console.log(error))
+}
+quotesFetcher()
+
+// let quotesArray = [];
+// (function quotesFetcher() {
+//     fetch('./ressources/quotes.json')
+//         .then((response) => response.json())
+//         .then((data) => {
+//             // quotesArray.push(data.quotes)
+//             quotesArray = data.quotes
+//             console.log(quotesArray)
+//         })
+//         .catch((error) => console.log(error))
+// }());
+
+const textModelContainer = document.querySelector('.text-model-container');
+function textModelDisplayer() {
+    const textModel = document.createElement('p');
+    textModel.setAttribute("id", "text-model");
+    textModelContainer.appendChild(textModel);
+
+    for (let i = 0; i < quote.length; i++) {
+        const letter = document.createElement('span');
+        letter.classList.add('letter-model');
+        letter.setAttribute("id", `letter${i}`);
+        letter.dataset.spanIndex = `letter${i}`;
+        letter.textContent = quote[i];
+        textModel.appendChild(letter);
+    };
+};
+
+let lettersIndex = 0;
+const textArea = document.getElementById('textArea');
+textArea.addEventListener('keydown', (e) => {
+    // console.log(e.target.value, e.key,quote[lettersIndex]);
+    sentenceChecker(e.key, e.target.value);
+});
+
+function sentenceChecker(key, textAreaValue) {
+    if (key === quote[lettersIndex] && textAreaValue === quote.slice(0, lettersIndex)) {
+        document.getElementById('letter' + lettersIndex).classList.remove('incorrect')
+        document.getElementById('letter' + lettersIndex).classList.add('correct')
+        lettersIndex++;
+        if (lettersIndex == quote.length) {
+            lettersIndex = 0;
+            function clearAllChildren(element) {
+                while (element.firstChild) {
+                    element.removeChild(element.firstChild);
+                }
+            }
+            clearAllChildren(textModelContainer);
+            setTimeout(() => {textArea.value = "";},100);
+            quotesFetcher();
+        }
+    } else {
+        document.getElementById('letter' + lettersIndex).classList.add('incorrect')
+    }
+}
+
+
+
+
+
+
+
